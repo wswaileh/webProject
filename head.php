9b7a6553ae91ef505ac2a57de93d4054a0d78edd
Waleed Swaileh <wsweilh@asaltech.com>
2019-05-03 20:59:48 +0300

    first commit

---
+++ head.php
@@ -0,0 +1,7 @@
+<head>
+    <title>LafLef</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Picnic Reservation Website">
+    <meta name="keywords" content="Picnics,LafLef">
+    <meta name="author" content="LafLef team">
+</head>
---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <?php require 'head.php'?>
+    <body>
+        <h1>LafLef!</h1>
+    </body>
+</html>
