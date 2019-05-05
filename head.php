c45f8167b0f130625a335c67abbeeee9da892896
Waleed Swaileh <wsweilh@asaltech.com>
2019-05-05 15:57:29 +0300

    removed my files

--- head.php
+++
@@ -1,7 +0,0 @@
-<head>
-    <title>LafLef</title>
-    <meta charset="UTF-8">
-    <meta name="description" content="Picnic Reservation Website">
-    <meta name="keywords" content="Picnics,LafLef">
-    <meta name="author" content="LafLef team">
-</head>
