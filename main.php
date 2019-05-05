50e9f7b7c268f8fe63a68613ad88bcf6883fb707
Waleed Swaileh <wsweilh@asaltech.com>
2019-05-05 16:12:58 +0300

    added footer file and controlled what appears in navbar based on userType

--- main.php
+++ main.php
@@ -1,14 +1,15 @@
 <?php
+session_name("name");
+session_start();
+$_SESSION['userType'] = 1; //GUEST USER
 include 'layout.php';
+include 'navbar.php';
     ?><nav><a href="customers.php">Customers</a><br>
     <a href="managers.php">Managers</a><br>
     <a href="picnics.php">Picnics</a><br>
     <a href="booking.php">Booking Dashboard</a><br>
-
-
 </nav>
-
-</body>
+<?php require 'footer.php'?>
 </html>
 
 <?
