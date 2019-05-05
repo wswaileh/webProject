619a9ca8f324a6e1bf5ebfb8076cec72d4de2d3c
Waleed Swaileh <wsweilh@asaltech.com>
2019-05-05 18:54:28 +0300

    responsive navbar

--- main.php
+++ main.php
@@ -3,13 +3,11 @@ session_name("name");
 session_start();
 $_SESSION['userType'] = 1; //GUEST USER
 include 'layout.php';
-
-    ?><nav><a href="customers.php">Customers</a><br>
-    <a href="managers.php">Managers</a><br>
-    <a href="picnics.php">Picnics</a><br>
-    <a href="booking.php">Booking Dashboard</a><br>
-</nav>
-<?php //require 'footer.php'?>
+?>
+    <nav>
+        <a href="customers.php">Customers</a><br>
+        <a href="managers.php">Managers</a><br>
+        <a href="picnics.php">Picnics</a><br>
+        <a href="booking.php">Booking Dashboard</a><br>
+    </nav>
 </html>
-
-<?
--- navbar.php
+++ navbar.php
@@ -4,38 +4,40 @@ if(!isset($_SESSION))
 {
     session_start();
 } ?>
-<nav class="nav">
-    <input type="checkbox" id="nav-check">
-    <div class="nav-header">
-        <div class="nav-title">
-            <a href="main.php" style="color:#FDFDFD;text-decoration: none">LafLef-لفلف</a>
-        </div>
-    </div>
-    <div class="nav-btn">
-        <label for="nav-check">
-            <span></span>
-            <span></span>
-            <span></span>
-        </label>
-    </div>
-
-    <div class="nav-links">
+<nav class="topnav" id="myTopnav">
+    <a href="#home" id="companyTitle">LafLef</a>
+    <div class="links">
+        <a href="main.php">Main</a>
         <?php
-        if ($_SESSION['userType'] === 1) //"GUEST"  LINKS NEED TO BE UPDATED
-            echo '<a href="main.php">About Us</a> 
-                  <a href="main.php">Register</a>
-                  <a href="main.php">Login</a>
-            ';
-        else if ($_SESSION['userType'] === 2) //"USER"      LINKS NEED TO BE UPDATED
-            echo '<a href="main.php">Main</a> 
-                  <a href="main.php">Logout</a>
-            ';
-        else //ADMIN        LINKS NEED TO BE UPDATED
-            echo '<a href="main.php">Main</a> 
-                  <a href="main.php">Add Picnic</a>
-                  <a href="main.php">Logout</a>
-            ';
-
+            if ($_SESSION['userType'] == 1) //GUEST         LINKS MUST BE UPDATED LATER
+                echo '
+                    <a href="main.php">About Us</a>
+                    <a href="main.php">Register</a>
+                    <a href="main.php">Login</a>
+                ';
+            else if ($_SESSION['userType'] == 2) //USER         LINKS MUST BE UPDATED LATER
+                echo '
+                    <a href="main.php">Logout</a>
+                ';
+            else
+                echo '
+                    <a href="main.php">Add Picnic</a>
+                    <a href="main.php">Logout</a>
+                ';
         ?>
     </div>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+        <i>▼</i>
+    </a>
 </nav>
+
+<script>
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
+</script>
