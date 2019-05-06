bb0fa2e62d5947c2a60ff114ab241211396507a6
Mustafa B3irat <mustafaadwi@gmail.com>
2019-05-06 18:49:36 +0300

    Worked on Booking added some styles

--- booking.php
+++ booking.php
@@ -50,11 +50,6 @@ include 'model.php';
                         $row[$i] = $desc[0];
                     }
 
-                    if (strpos($i, "time")) {
-                        $time = explode(":", $row[$i]);
-                        unset($time[2]);
-                        $row[$i] = implode(":", $time);
-                    }
                     if ($i == "pid") {
                         $hidden["pid"] = $row[$i];
                         echo "<td style='padding-left: 80px;font-size: 1.4pc'><a href='detailed.php?id=" . $row[$i] . "' style='text-decoration:none;'>" . $row[$i] . "</a></td>";
@@ -62,6 +57,14 @@ include 'model.php';
                         $hidden["cost"] = $row[$i];
                         ?>
                         <td style="padding-left: 40px"><?= $row[$i] ?> &#8362;</td><?php
+                    } else if (strpos($i, "time")) {
+                        $time = explode(":", $row[$i]);
+                        unset($time[2]);
+                        $row[$i] = implode(":", $time);
+
+                        ?>
+                        <td style="padding-left: 20px">
+                        <time><?= $row[$i] ?></time></td><?php
                     } else { ?>
                         <td><?= $row[$i] ?></td><?php
                     }
@@ -74,15 +77,27 @@ include 'model.php';
 
             <tr>
 
-                <td colspan="4"><input type="text" class="filter-input"
-                                       name="additions" id="additions"
-                                       placeholder="If you want ant additions state it here... e.g Birthday cake"></td>
+                <td colspan="1"><label style="font-size: 16px">Additions~Birthday cake:</label></td>
+                <td colspan="1" style="padding-left: 20px">
+                    <div class="tooltip"><input type="checkbox" name="Birthday_cake" value="Birthday Cake">
+                        <span class="tooltiptext">Check the box if you want to add birthday cake to your order!</span>
+                    </div>
+                </td>
+                <td colspan="1">
+                    <div class="tooltip"><input type="number" class="filter-input"
+                                                name="cakeNum" id="cakeNum" placeholder="#People">
+                        <span class="tooltiptext">For how many people do you want the cake?</span>
+                    </div>
+                </td>
+                <td colspan="1">
 
+                </td>
                 <td colspan="3"><input type="number" min="1" max="2"
                                        class="filter-input"
                                        name="numOfSeats"
                                        placeholder="Please Enter Number of People intend to come "
                                        id="NumberOfPeople"></td>
+
                 <td colspan="2"><input style="width: 200px" type="submit" name="confirm" value="Confirm The Booking"
                                        class="button"><input type="hidden" value="<?= $hidden["pid"] ?>"
                                                              name="pid"><input type="hidden"
