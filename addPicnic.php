df545d48e603e405a67ed7a89f68733c6c018f95
Mustafa B3irat <mustafaadwi@gmail.com>
2019-05-13 00:14:17 +0300

    Huge update!

--- addPicnic.php
+++ addPicnic.php
@@ -5,121 +5,125 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-	?>
-
- <link rel="stylesheet" href="css/addpicnic.css" type="text/css">
+?>
 
 
+    <html>
+    <head>
+        <title>Add picnic</title>
+        <script src="ckeditor/ckeditor.js"></script>
 
-<html>
-<head>
-	<title>Add picnic</title>
-</head>
-<body>
-	
-
-<div class="form">
-	 <h2 id="h"> Add Picnic</h2>
-    <form action="#" method="POST" enctype="multipart/form-data">
-     
-      <label>Title : </label><input type="text" name="title"
-      placeholder="Picnic Ttile"  required="" />
-
-      <label>Place :</label><input type="text" name="place" placeholder="Picnic Place" required="" />
-
-      <label>Price: </label><input type="number" name="price"
-      placeholder="Cost Per Person"  step="any" min="0" max="1000" required="" />	
+        <link rel="stylesheet" href="css/addpicnic.css" type="text/css">
 
 
-       <label>capacity : </label><input type="number" name="capacity"
-      placeholder="Picnic Capacity" step="1" min="20" max="50" required="" />
+    </head>
+    <body>
 
 
-     <label>Description : </label><textarea  name="description"
-      placeholder="Picnic Description"  required=""  rows="3" cols="64"></textarea>
+    <div class="form">
+        <h2 id="h"> Add Picnic</h2>
+        <form action="#" method="POST" enctype="multipart/form-data">
 
-    
-        <label>Food : </label><textarea  name="food"
-      placeholder="Picnic Food"  required=""  rows="3" cols="64"></textarea>
+            <label>Title : </label><input type="text" name="title"
+                                          placeholder="Picnic Ttile" required=""/>
 
-        <label>Departual place : </label><input type="text" name="departurelocation"
-      placeholder="Picnic Departual place"  required="" />
+            <label>Place :</label><input type="text" name="place" placeholder="Picnic Place" required=""/>
 
- 	<label>Departual time : </label><input type="time" name="departuretime"
-      placeholder="Picnic Departual time"  required="" />
-
-      <label>arrival time : </label><input type="time" name="arrivaltime"
-      placeholder="Picnic arrival time"  required="" />
-
-      <label>return time : </label><input type="time" name="returntime"
-      placeholder="Picnic return time"  required="" />
-
-      <label>Date : </label><input type="date" name="date"
-      placeholder="Picnic  Date"  required="" />
-
-        <label>image1 : </label><input type="file" name="image1" />
-        <label>image2 : </label><input type="file" name="image2" />
-        <label>image3 : </label><input type="file" name="image3" />
-    
+            <label>Price: </label><input type="number" name="price"
+                                         placeholder="Cost Per Person" step="any" min="0" max="1000" required=""/>
 
 
- 		 <label>Activities : </label><textarea  name="activities"
-      placeholder="Picnic Activities"  required=""  rows="3" cols="64"></textarea>
+            <label>capacity : </label><input type="number" name="capacity"
+                                             placeholder="Picnic Capacity" step="1" min="20" max="50" required=""/>
 
 
+            <label>Food : </label><textarea name="food"
+                                            placeholder="Picnic Food" required="" rows="3" cols="64"></textarea>
+
+            <label>Departual place : </label><input type="text" name="departurelocation"
+                                                    placeholder="Picnic Departual place" required=""/>
+
+            <label>Departual time : </label><input type="time" name="departuretime"
+                                                   placeholder="Picnic Departual time" required=""/>
+
+            <label>arrival time : </label><input type="time" name="arrivaltime"
+                                                 placeholder="Picnic arrival time" required=""/>
+
+            <label>return time : </label><input type="time" name="returntime"
+                                                placeholder="Picnic return time" required=""/>
+
+            <label>Date : </label><input type="date" name="date"
+                                         placeholder="Picnic  Date" required=""/>
+
+            <label>image1 : </label><input type="file" name="image1"/>
+            <label>image2 : </label><input type="file" name="image2"/>
+            <label>image3 : </label><input type="file" name="image3"/>
 
 
-      <input type="Submit" name="Submit" value="Submit"/>  
-    </form>
-  </div>
+            <label>Activities : </label><textarea name="activities"
+                                                  placeholder="Picnic Activities" required="" rows="3"
+                                                  cols="64" class="card__img"></textarea>
 
 
-</body>
-</html>
+            <textarea name="description" class="description" placeholder="Picnic Description"
+                      required=""></textarea>
+
+            <label class="description-label" id="description-label"
+                   onclick="document.getElementById('description-label').style.display='none';">Picnic
+                Description</label>
+            <input type="Submit" name="Submit" value="Submit"/>
+        </form>
+    </div>
+
+    <script>
+        CKEDITOR.replace('description', {
+            extraPlugins: 'placeholder',
+            height: 220
+        });
+    </script>
+
+    </body>
+    </html>
 
 
+<?php
 
- <?php
 
-	
 include 'model.php';
-	
-if(!empty($_POST)) {
 
-  $add = addPicnic($_POST['title'],$_POST['place'], $_POST['price'], $_POST['capacity'],$_POST['description'] , $_POST['food'], $_POST['departurelocation'],$_POST['departuretime'] , $_POST['arrivaltime'],$_POST['returntime'],$_POST['date'],$_POST['activities']);
+if (!empty($_POST)) {
 
-	if($add == 1) {
+
+        $add = addPicnic($_POST['title'], $_POST['place'], $_POST['price'], $_POST['capacity'], $_POST['description'], $_POST['food'], $_POST['departurelocation'], $_POST['departuretime'], $_POST['arrivaltime'], $_POST['returntime'], $_POST['date'], $_POST['activities']);
+
+        if ($add == 1) {
 
 ############################################################upload images
-		
-		$id = getIdForLastPicnic();
-		$target_file = 'picnics/'.$id."_1".".".pathinfo( $_FILES['image1']['name'], PATHINFO_EXTENSION) ;
- move_uploaded_file($_FILES["image1"]["tmp_name"], $target_file);
- 
 
- $target_file = 'picnics/'.$id."_2".".".pathinfo( $_FILES['image2']['name'], PATHINFO_EXTENSION) ;
- move_uploaded_file($_FILES["image2"]["tmp_name"], $target_file);
- 
+            $id = getIdForLastPicnic();
 
- $target_file = 'picnics/'.$id."_3".".".pathinfo( $_FILES['image3']['name'], PATHINFO_EXTENSION);
- move_uploaded_file($_FILES["image3"]["tmp_name"], $target_file);
-       
+            $img = [$id . "_1", $id . "_2", $id . "_3"];
+            $imgStr = implode(';', $img);
+
+            $target_file = 'img/picnics/' . $id . "_1" . "." . pathinfo($_FILES['image1']['name'], PATHINFO_EXTENSION);
+            move_uploaded_file($_FILES["image1"]["tmp_name"], $target_file);
 
 
+            $target_file = 'img/picnics/' . $id . "_2" . "." . pathinfo($_FILES['image2']['name'], PATHINFO_EXTENSION);
+            move_uploaded_file($_FILES["image2"]["tmp_name"], $target_file);
+
+
+            $target_file = 'img/picnics/' . $id . "_3" . "." . pathinfo($_FILES['image3']['name'], PATHINFO_EXTENSION);
+            move_uploaded_file($_FILES["image3"]["tmp_name"], $target_file);
+
+
+            addImagesToPicnic($id, $imgStr);
 
 ####################################################################
 
-		echo ("<script LANGUAGE='JavaScript'>
-    #window.alert('User added succifully');
-    #window.location.href='main.php';
-    #</script>");
-	}
-	else {
-		echo ("<script LANGUAGE='JavaScript'>
-    window.alert('Error 404');
-    window.location.href='addPicnic.php';
-    </script>");
-	}
-	}
-		
+            echo "<script type='text/javascript'>alert('Picnic Added Successfully!')</script>";
+
+        }
+
+}
 ?>
--- registerConfirm.php
+++ registerConfirm.php
@@ -20,28 +20,31 @@ error_reporting(E_ALL);
 <div class="form">
     <form class="form1" action="" method="post">
         <h2 id="h"> register Confirmation</h2>
-        <label> NAME :</label> <input type="text" name="name"  required=""
+        <label> NAME :</label> <input type="text" name="name" required=""
                                       value="<?php echo(isset($_SESSION['name']) ? $_SESSION['name'] : ''); ?>"/><br>
 
-        <label>EMAIL :</label> <input type="email" name="email"  required=""
+        <label>EMAIL :</label> <input type="email" name="email" required=""
                                       value="<?php echo(isset($_SESSION['emailr']) ? $_SESSION['emailr'] : ''); ?>"/>
-                                      <?php if (isset($_SESSION['error']) && $_SESSION['errorc']== 1) echo "<sup style=" . "color:red;margin-left:180px;" . ";> email already exist </sup>"; ?>
+        <?php if (isset($_SESSION['error']) && $_SESSION['errorc'] == 1) echo "<sup style=" . "color:red;margin-left:180px;" . ";> email already exist </sup>"; ?>
         <br>
 
-        <label>Username :</label> <input type="text" name="username"  required=""  value="<?php echo(isset($_SESSION['username']) ? $_SESSION['username'] : ''); ?>"/>
-          <?php if (isset($_SESSION['errorc'])){ if( $_SESSION['errorc']== 2) echo "
-  <sup style=" . "color:red;margin-left:180px;" . ";> invalid username , number of characters<br><span style="."margin-left:180px;" .">should be between 6-13</span></sup> ";
-  elseif( $_SESSION['errorc']== 3)echo "<sup style=" . "color:red;margin-left:180px;" . ";> used username </sup>"; }
-  ?>
+        <label>Username :</label> <input type="text" name="username" required=""
+                                         value="<?php echo(isset($_SESSION['username']) ? $_SESSION['username'] : ''); ?>"/>
+        <?php if (isset($_SESSION['errorc'])) {
+            if ($_SESSION['errorc'] == 2) echo "
+  <sup style=" . "color:red;margin-left:180px;" . ";> invalid username , number of characters<br><span style=" . "margin-left:180px;" . ">should be between 6-13</span></sup> ";
+            elseif ($_SESSION['errorc'] == 3) echo "<sup style=" . "color:red;margin-left:180px;" . ";> used username </sup>";
+        }
+        ?>
 
-  <label>id :</label><input type="text" name="phone" 
-                                     value="<?php echo(isset($_SESSION['id']) ? $_SESSION['id'] : ''); ?>" readonly /><br>
+        <label>id :</label><input type="text" name="phone"
+                                  value="<?php echo(isset($_SESSION['id']) ? $_SESSION['id'] : ''); ?>" readonly/><br>
 
         <label>PHONE :</label><input type="text" name="phone" required=""
                                      value="<?php echo(isset($_SESSION['phone']) ? $_SESSION['phone'] : ''); ?>"/><br>
 
-        
-        <label>Address :</label> <input type="text" name="address"  required=""
+
+        <label>Address :</label> <input type="text" name="address" required=""
                                         value="<?php echo(isset($_SESSION['address']) ? $_SESSION['address'] : ''); ?>"/><br>
 
         <label>DOB :</label><input type="date" name="dob" placeholder="Enter birth of date" required=""
@@ -49,9 +52,8 @@ error_reporting(E_ALL);
         <button class="button" type="submit">Confirm
         </button>
 
-         <?php if (isset($_SESSION['added']) && $_SESSION['added'] == 0)  echo "<br><br><p style=" . "color:red;margin-left:180px;" . ";> Error 402</p>"; ?>
+        <?php if (isset($_SESSION['added']) && $_SESSION['added'] == 0) echo "<br><br><p style=" . "color:red;margin-left:180px;" . ";> Error 402</p>"; ?>
 
-         
 
     </form>
 </div>
@@ -61,40 +63,36 @@ error_reporting(E_ALL);
 </html>
 
 
-
 <?php
 
-include 'registerValidation.php' ;
+include 'registerValidation.php';
 if (!empty($_POST)) {
 
-      $_SESSION['emailr'] = $_POST['email']; 
-      $_SESSION['dob'] = $_POST['dob'];
-      $_SESSION['address'] = $_POST['address']; 
-      $_SESSION['phone'] = $_POST['phone'];
-      $_SESSION['name'] = $_POST['name'];
-      $_SESSION['username'] = $_POST['username'] ;
- 
+    $_SESSION['emailr'] = $_POST['email'];
+    $_SESSION['dob'] = $_POST['dob'];
+    $_SESSION['address'] = $_POST['address'];
+    $_SESSION['phone'] = $_POST['phone'];
+    $_SESSION['name'] = $_POST['name'];
+    $_SESSION['username'] = $_POST['username'];
 
-    $error = finalCheck($_SESSION['emailr'] , $_SESSION['username']) ;
-    if ($error != 0 ) {
-       unset($_SESSION['added']);
-       $_SESSION['errorc']= $error ; 
+
+    $error = finalCheck($_SESSION['emailr'], $_SESSION['username']);
+    if ($error != 0) {
+        unset($_SESSION['added']);
+        $_SESSION['errorc'] = $error;
         header('Location: registerConfirm.php');
-    }
-
-        elseif (addUser($_SESSION['name'], $_SESSION['emailr'],$_SESSION['username'], $_SESSION['phone'], $_SESSION['password'], $_SESSION['address'], $_SESSION['dob'],$_SESSION['id']) == 1){
-          $_SESSION['userType'] = 2;
-         echo ("<script LANGUAGE='JavaScript'>
+    } elseif (addUser($_SESSION['name'], $_SESSION['emailr'], $_SESSION['username'], $_SESSION['phone'], md5($_SESSION['password']), $_SESSION['address'], $_SESSION['dob'], $_SESSION['id']) == 1) {
+        $_SESSION['userType'] = 2;
+        echo("<script LANGUAGE='JavaScript'>
     window.alert('User added succifully');
     window.location.href='main.php';
     </script>");
-    }
-    else  {
-      $_SESSION['added'] = 0 ; 
-      unset($_SESSION['errorc']);
+    } else {
+        $_SESSION['added'] = 0;
+        unset($_SESSION['errorc']);
         header('Location: registerConfirm.php');
     }
-     
+
 
 }
 
